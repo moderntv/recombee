a7b6b78243754c3a7f1671a9726b2aae373f5f75
Stanislav Zeman <zeman@standa.dev>
2023-11-24 12:55:19 +0100

    feat: implement general segmentation requests

---
+++ segmentation.go
@@ -0,0 +1,33 @@
+package recombee
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// ListSegmentations return all existing items Segmentations.
+func ListSegmentations(sourceType string) Request {
+	params := make(map[string]interface{})
+	params["sourceType"] = sourceType
+	return Request{
+		Path:   "/segmentations/list/",
+		Method: http.MethodGet,
+		Params: params,
+	}
+}
+
+// GetSegmentation gets existing Segmentation.
+func GetSegmentation(segmentationId string) Request {
+	return Request{
+		Path:   fmt.Sprintf("/segmentations/list/%s", segmentationId),
+		Method: http.MethodGet,
+	}
+}
+
+// DeleteSegmentation deletes existing Segmentation.
+func DeleteSegmentation(segmentationId string) Request {
+	return Request{
+		Path:   fmt.Sprintf("/segmentations/list/%s", segmentationId),
+		Method: http.MethodDelete,
+	}
+}
