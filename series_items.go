b6582c06f380bc7aa58b6b99afbe0145bbba3cc4
Stanislav Zeman <zeman@standa.dev>
2023-11-30 16:44:48 +0100

    style: move function definition to comply with recombee documentation

--- series_items.go
+++ series_items.go
@@ -5,6 +5,14 @@ import (
 	"net/http"
 )
 
+// ListSeriesItems lists all the items present in the given series, sorted according to their time index values.
+func ListSeriesItems(seriesId string) Request {
+	return Request{
+		Path:   fmt.Sprintf("/series/%s/items/", seriesId),
+		Method: http.MethodGet,
+	}
+}
+
 // InsertToSeries inserts an existing item/series into a series of the given seriesId at a position determined by time.
 func InsertToSeries(seriesId string, itemType string, itemId string, time_ float64, opts ...RequestOption) Request {
 	params := make(map[string]interface{})
@@ -28,18 +36,9 @@ func RemoveFromSeries(seriesId string, itemType string, itemId string, time_ flo
 	params["itemType"] = itemType
 	params["itemId"] = itemId
 	params["time"] = time_
-
 	return Request{
 		Path:   fmt.Sprintf("/series/%s/items/", seriesId),
 		Method: http.MethodDelete,
 		Params: params,
-	}
-}
-
-// ListSeriesItems lists all the items present in the given series, sorted according to their time index values.
-func ListSeriesItems(seriesId string) Request {
-	return Request{
-		Path:   fmt.Sprintf("/series/%s/items/", seriesId),
-		Method: http.MethodGet,
 	}
 }
